641f9850a4d293057c9eab77713fd33aecef33d7
alexv7 <alexvarjabedian7@gmail.com>
2016-08-12 16:26:59 -0400

    in the previous commit, we had created a sale action in home controller, I rename the action to sakhat and now added the route for it in config/routes.rb

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
   HomeController.class_eval do
-    def sale
+    def sakhat
       @products = Product.joins(:variants_including_master).where('spree_variants.sakhat_price is not null').uniq
     end
   end
