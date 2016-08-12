562df5826b1db301dfe63e9221896da2caf6630c
alexv7 <alexvarjabedian7@gmail.com>
2016-08-12 16:23:41 -0400

    added some code in home_controller_decorator.rb to select sakhat prices...we are trying to create a view to show all variants that have sakhat prices

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,1 +1,7 @@
-home_controller_decorator.rb
+module Spree
+  HomeController.class_eval do
+    def sale
+      @products = Product.joins(:variants_including_master).where('spree_variants.sakhat_price is not null').uniq
+    end
+  end
+end
