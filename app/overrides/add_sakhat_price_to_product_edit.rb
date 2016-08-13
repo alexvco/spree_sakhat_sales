bc8cbc310b1860a453c40e4fe9e80b8fa1370d0f
alexv7 <alexvarjabedian7@gmail.com>
2016-08-12 20:00:34 -0400

    playing with deface

--- app/overrides/add_sakhat_price_to_product_edit.rb
+++ app/overrides/add_sakhat_price_to_product_edit.rb
@@ -12,16 +12,16 @@
 
 Deface::Override.new(:virtual_path => 'spree/admin/products/_form',
   :name => 'add_sale_price_to_product_edit',
-  :replace => "div data-hook='admin_product_form_price'",
+  :replace => "[data-hook='admin_product_form_price']",
   :text => "
 
-      <div data-hook='admin_product_form_price'>
-        <%= f.field_container :price, class: ['form-group'] do %>
-          <%= f.label :price, raw(Spree.t(:master_price) + content_tag(:span, ' *', class: 'required')) %>
-          <%= f.text_field :price, value: number_to_currency(@product.price, unit: ''), class: 'form-control', disabled: (cannot? :update, Spree::Price) %>
-          <%= f.error_message_on :price %>
-        <% end %>
-      </div>
+    <div data-hook='admin_product_form_price'>
+      <%= f.field_container :price, class: ['form-group'] do %>
+        <%= f.label :price, raw(Spree.t(:master_price) + content_tag(:span, ' *', class: 'required')) %>
+        <%= f.text_field :price, value: number_to_currency(@product.price, unit: ''), class: 'form-control', disabled: (cannot? :update, Spree::Price) %>
+        <%= f.error_message_on :price %>
+      <% end %>
+    </div>
 
     <div data-hook='admin_product_form_sakhat_price'>
       <%= f.field_container :sakhat_price class: ['form-group'] do %>
