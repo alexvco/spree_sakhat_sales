1e67fdad8ddc0df11ae21b7de3cffc73f6dbb170
alexv7 <alexvarjabedian7@gmail.com>
2016-08-12 20:10:55 -0400

    deface again

--- app/overrides/add_sakhat_price_to_product_edit.rb
+++ app/overrides/add_sakhat_price_to_product_edit.rb
@@ -15,6 +15,12 @@ Deface::Override.new(:virtual_path => 'spree/admin/products/_form',
   :replace => "[data-hook='admin_product_form_price']",
   :text => "
 
-      <p>Testing</p>
+    <div data-hook='admin_product_form_price'>
+      <%= f.field_container :price, class: ['form-group'] do %>
+        <%= f.label :price, raw(Spree.t(:master_price) + content_tag(:span, ' *', class: 'required')) %>
+        <%= f.text_field :price, value: number_to_currency(@product.price, unit: ''), class: 'form-control', disabled: (cannot? :update, Spree::Price) %>
+        <%= f.error_message_on :price %>
+      <% end %>
+    </div>
     
   ")
