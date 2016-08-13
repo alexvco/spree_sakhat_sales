bd512397fb5518bb4ca761f590df09ef7ead8c42
alexv7 <alexvarjabedian7@gmail.com>
2016-08-12 20:36:02 -0400

    commented out deface changes, because i want to do them from the app...testing if it will work

--- app/overrides/add_sakhat_price_to_product_edit.rb
+++ app/overrides/add_sakhat_price_to_product_edit.rb
@@ -1,3 +1,5 @@
+# Deface changes from tutorial
+
 # Deface::Override.new(:virtual_path => 'spree/admin/products/_form',
 #   :name => 'add_sale_price_to_product_edit',
 #   :insert_after => "erb[loud]:contains('text_field :price')",
@@ -10,24 +12,27 @@
 #     <% end %>
 #   ")
 
-Deface::Override.new(:virtual_path => 'spree/admin/products/_form',
-  :name => 'add_sale_price_to_product_edit',
-  :replace => "[data-hook='admin_product_form_price']",
-  :text => "
 
-    <div data-hook='admin_product_form_price'>
-      <%= f.field_container :price, class: ['form-group'] do %>
-        <%= f.label :price, raw(Spree.t(:master_price) + content_tag(:span, ' *', class: 'required')) %>
-        <%= f.text_field :price, value: number_to_currency(@product.price, unit: ''), class: 'form-control', disabled: (cannot? :update, Spree::Price) %>
-        <%= f.error_message_on :price %>
-      <% end %>
-    </div>
+# My Deface changes
+
+# Deface::Override.new(:virtual_path => 'spree/admin/products/_form',
+#   :name => 'add_sale_price_to_product_edit',
+#   :replace => "[data-hook='admin_product_form_price']",
+#   :text => "
+
+#     <div data-hook='admin_product_form_price'>
+#       <%= f.field_container :price, class: ['form-group'] do %>
+#         <%= f.label :price, raw(Spree.t(:master_price) + content_tag(:span, ' *', class: 'required')) %>
+#         <%= f.text_field :price, value: number_to_currency(@product.price, unit: ''), class: 'form-control', disabled: (cannot? :update, Spree::Price) %>
+#         <%= f.error_message_on :price %>
+#       <% end %>
+#     </div>
     
-    <div data-hook='admin_product_form_price'>
-      <%= f.field_container :sakhat_price, class: ['form-group'] do %>
-        <%= f.label :sakhat_price, raw(Spree.t(:sakhat_price) + content_tag(:span, ' *')) %>
-        <%= f.text_field :sakhat_price, :value => number_to_currency(@product.sakhat_price, :unit => ''), class: 'form-control' %>
-        <%= f.error_message_on :sakhat_price %>
-      <% end %>
-    </div>
-  ")
+#     <div data-hook='admin_product_form_price'>
+#       <%= f.field_container :sakhat_price, class: ['form-group'] do %>
+#         <%= f.label :sakhat_price, raw(Spree.t(:sakhat_price) + content_tag(:span, ' *')) %>
+#         <%= f.text_field :sakhat_price, :value => number_to_currency(@product.sakhat_price, :unit => ''), class: 'form-control' %>
+#         <%= f.error_message_on :sakhat_price %>
+#       <% end %>
+#     </div>
+#   ")
